644965d58cf7427e5107463704b106cbfc2d2733
Thai Ngoc Tu <tu.thai@gnomons.com>
2024-09-16 10:21:50 +0700

    chore: Remove unnecessary files

--- applications/tflm_person_detection/testdata/no_person_image_data.h
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-constexpr unsigned int g_no_person_image_data_size = 9216;
-extern const unsigned char g_no_person_image_data[];
--- applications/tflm_person_detection/testdata/person_image_data.h
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-constexpr unsigned int g_person_image_data_size = 9216;
-extern const unsigned char g_person_image_data[];
